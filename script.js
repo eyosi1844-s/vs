f687af8cf7f035ea267ae73d70605006e4e7ff6d
eyosi <eyosibekele4@gmail.com>
2024-12-24 01:47:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs' } });
 
-let editor;    // Store Monaco Editor instance
-let files = [];  // Array to store the file data
+let editor;
 
 require(['vs/editor/editor.main'], function () {
-  // Initialize Monaco Editor
   editor = monaco.editor.create(document.getElementById('editor'), {
-    value: '',  // Empty editor content by default
-    language: 'javascript',  // Default language is JavaScript
-    theme: 'vs-dark',  // Set dark theme
+    value: "// Write your code here",
+    language: 'javascript',
+    theme: 'vs-dark',
   });
-  
-  // Load initial file list
-  loadFileList();
 });
